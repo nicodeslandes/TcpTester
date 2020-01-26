88b933a722ab15bd9513a7eb2e5e404bda16622e
Nicolas Deslandes <ndeslandes@live.fr>
2020-01-26 11:55:28 +0000

    Project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TcpTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
